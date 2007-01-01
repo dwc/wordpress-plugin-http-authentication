50ca40605da28fffe8cac7189a2720710ab3b9d7
Daniel Westermann-Clark <dwc@pobox.com>
2007-01-01 20:14:12 +0000

    Bump

--- http-authentication.php
+++ http-authentication.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: HTTP Authentication
-Version: 1.5
+Version: 1.6
 Plugin URI: http://dev.webadmin.ufl.edu/~dwc/2005/03/10/http-authentication-plugin/
 Description: Authenticate users using basic HTTP authentication (<code>REMOTE_USER</code>). This plugin assumes users are externally authenticated, as with <a href="http://www.gatorlink.ufl.edu/">GatorLink</a>.
 Author: Daniel Westermann-Clark
