ccef8ddd0711eea5f8fdc7835b3b41512057f8b6
Daniel Westermann-Clark <dwc@pobox.com>
2009-08-20 19:29:45 +0000

    - Add support for WordPress MU (Elliot Kendall) - Allow for mixed HTTP and built-in authentication by falling back to   wp-login.php (Elliot Kendall)

--- http-authentication.php
+++ http-authentication.php
@@ -9,19 +9,47 @@ Author URI: http://dev.webadmin.ufl.edu/~dwc/
 */
 
 if (! class_exists('HTTPAuthenticationPlugin')) {
+	$wpmu;
+	$username;
+
 	class HTTPAuthenticationPlugin {
 		function HTTPAuthenticationPlugin() {
 			register_activation_hook(__FILE__, array(&$this, 'initialize_options'));
 
+			$this->username = '';
+			foreach (array('REMOTE_USER', 'REDIRECT_REMOTE_USER') as $key) {
+				if (isset($_SERVER[$key])) {
+					$this->username = $_SERVER[$key];
+				}
+			}
+
+			// Allow fallthrough for non-protected blogs
+			if ($this->username !== '') {
+				add_action('wp_authenticate', array(&$this, 'authenticate'), 10, 2);
+				add_filter('check_password', array(&$this, 'skip_password_check'), 10, 4);
+			}
+
 			add_action('admin_menu', array(&$this, 'add_options_page'));
-			add_action('wp_authenticate', array(&$this, 'authenticate'), 10, 2);
-			add_filter('check_password', array(&$this, 'skip_password_check'), 10, 4);
 			add_action('wp_logout', array(&$this, 'logout'));
 			add_action('lost_password', array(&$this, 'disable_function'));
 			add_action('retrieve_password', array(&$this, 'disable_function'));
 			add_action('password_reset', array(&$this, 'disable_function'));
 			add_action('check_passwords', array(&$this, 'generate_password'), 10, 3);
 			add_filter('show_password_fields', array(&$this, 'disable_password_fields'));
+
+			// WORDPRESS MU DETECTION
+			//    0 - Regular WordPress installation
+			//    1 - WordPress MU forced activation
+			//    2 - WordPress MU optional activation
+			$this->wpmu = 0;
+			if (basename(dirname(__FILE__)) == "mu-plugins") {
+				// Forced activation
+				$this->wpmu = 1;
+			}
+			else if (basename(dirname(__FILE__)) == "plugins" && function_exists('is_site_admin')) {
+				// Optional activation
+				$this->wpmu = 2;
+			}
 		}
 
 
@@ -34,7 +62,7 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 */
 		function initialize_options() {
 			if (current_user_can('manage_options')) {
-				add_option('http_authentication_logout_uri', get_option('home'), 'The URI to which the user is redirected when she chooses "Logout".');
+				add_option('http_authentication_logout_uri', $this->get_option('home'), 'The URI to which the user is redirected when she chooses "Logout".');
 				add_option('http_authentication_auto_create_user', false, 'Should a new user be created automatically if not already in the WordPress database?');
 				add_option('http_authentication_auto_create_email_domain', '', 'The domain to use for the email address of an automatically created user.');
 			}
@@ -45,7 +73,13 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 */
 		function add_options_page() {
 			if (function_exists('add_options_page')) {
-				add_options_page('HTTP Authentication', 'HTTP Authentication', 9, __FILE__, array(&$this, '_display_options_page'));
+				if ($this->wpmu == 1 && function_exists('is_site_admin') && is_site_admin()) {
+					add_submenu_page('wpmu-admin.php', 'HTTP Authentication', 'HTTP Authentication', 'manage_options', __FILE__, array(&$this, '_display_options_page'));
+					add_options_page('HTTP Authentication', 'HTTP Authentication', 9, __FILE__, array(&$this, '_display_options_page'));
+				}
+				else if ($this->wpmu != 1) {
+					add_options_page('HTTP Authentication', 'HTTP Authentication', 9, __FILE__, array(&$this, '_display_options_page'));
+				}
 			}
 		}
 
@@ -55,16 +89,7 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 * you have externally authenticated the user.
 		 */
 		function authenticate($username, $password) {
-			$username = '';
-			if (isset($_SERVER['REMOTE_USER'])) {
-				$username = $_SERVER['REMOTE_USER'];
-			}
-			elseif (isset($_SERVER['REDIRECT_REMOTE_USER'])) {
-				$username = $_SERVER['REDIRECT_REMOTE_USER'];
-			}
-			else {
-				die('No REMOTE_USER or REDIRECT_REMOTE_USER found; please check your external authentication configuration');
-			}
+			$username = $this->username;
 
 			// Fake WordPress into authenticating by overriding the credentials
 			$password = $this->_get_password();
@@ -93,7 +118,7 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 * Logout the user by redirecting them to the logout URI.
 		 */
 		function logout() {
-			header('Location: ' . get_option('http_authentication_logout_uri'));
+			header('Location: ' . $this->get_option('http_authentication_logout_uri'));
 			exit();
 		}
 
@@ -128,6 +153,30 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 *************************************************************/
 
 		/*
+		 * Use the appropriate get_option function for our MU config.
+		 */
+		function get_option($option) {
+			if ($this->wpmu == 1) {
+				return get_site_option($option);
+			}
+			else {
+				return get_option($option);
+			}
+		}
+
+		/*
+		 * Use the appropriate update_option function for our MU config.
+		 */
+		function update_option($option, $value) {
+			if ($this->wpmu == 1) {
+				update_site_option($option, $value);
+			}
+			else {
+				update_option($option, $value);
+			}
+		}
+
+		/*
 		 * Generate a random password.
 		 */
 		function _get_password($length = 10) {
@@ -139,7 +188,7 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 */
 		function _create_user($username) {
 			$password = $this->_get_password();
-			$email_domain = get_option('http_authentication_auto_create_email_domain');
+			$email_domain = $this->get_option('http_authentication_auto_create_email_domain');
 
 			require_once(WPINC . DIRECTORY_SEPARATOR . 'registration.php');
 			wp_create_user($username, $password, $username . ($email_domain ? '@' . $email_domain : ''));
@@ -149,13 +198,27 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
 		 * Display the options for this plugin.
 		 */
 		function _display_options_page() {
-			$logout_uri = get_option('http_authentication_logout_uri');
-			$auto_create_user = (bool) get_option('http_authentication_auto_create_user');
-			$auto_create_email_domain = get_option('http_authentication_auto_create_email_domain');
+			// Update any options that were set
+			foreach (array('logout_uri', 'auto_create_email_domain') as $key) {
+				$key = "http_authentication_$key";
+				if (isset($_POST[$key])) {
+					$this->update_option($key, trim($_POST[$key]));
+				}
+			}
+
+			// Update boolean option (if unchecked, it doesn't get set)
+			if ($_POST) {
+				$key = 'http_authentication_auto_create_user';
+				$this->update_option($key, (bool) $_POST[$key]);
+			}
+
+			$logout_uri = $this->get_option('http_authentication_logout_uri');
+			$auto_create_user = (bool) $this->get_option('http_authentication_auto_create_user');
+			$auto_create_email_domain = $this->get_option('http_authentication_auto_create_email_domain');
 ?>
 <div class="wrap">
   <h2>HTTP Authentication Options</h2>
-  <form action="options.php" method="post">
+  <form method="post" action="<?php echo $_SERVER['REDIRECT_SCRIPT_URI'] . '?page=' . plugin_basename(__FILE__); ?>&updated=true">
     <input type="hidden" name="action" value="update" />
     <input type="hidden" name="page_options" value="http_authentication_logout_uri,http_authentication_auto_create_user,http_authentication_auto_create_email_domain" />
     <?php if (function_exists('wp_nonce_field')): wp_nonce_field('update-options'); endif; ?>
@@ -165,7 +228,7 @@ if (! class_exists('HTTPAuthenticationPlugin')) {
         <th scope="row"><label for="http_authentication_logout_uri">Logout URI</label></th>
         <td>
           <input type="text" name="http_authentication_logout_uri" id="http_authentication_logout_uri" value="<?php echo htmlspecialchars($logout_uri) ?>" size="50" /><br />
-          Default is <code><?php echo htmlspecialchars(get_option('home')); ?></code>; override to e.g. remove a cookie.
+          Default is <code><?php echo htmlspecialchars($this->get_option('home')); ?></code>; override to e.g. remove a cookie.
         </td>
       </tr>
       <tr valign="top">
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: dwc
 Tags: authentication
 Requires at least: 2.5.1
-Tested up to: 2.7
+Tested up to: 2.8.4
 Stable tag: 2.3
 
 Use an external authentication source in WordPress.
@@ -22,6 +22,8 @@ The HTTP Authentication plugin allows you to use existing means of authenticatin
 7. Try logging in as one of the users added in step 4.
 
 Note: This version works with WordPress 2.5.1 and above. Use [version 1.8](http://downloads.wordpress.org/plugin/http-authentication.1.8.zip) if you are using WordPress 2.0.
+
+This plugin also supports WordPress MU as of version 2.4. You can install it globally or on a per-site basis. Refer to the WordPress MU documentation for more information on installing plugins.
 
 == Frequently Asked Questions ==
 
@@ -72,3 +74,9 @@ Because this plugin generates a random password when you create a new user or ed
 Also, you should leave the `admin` user as a fallback, i.e. create a new account to use with this plugin. As long as you don't edit the `admin` profile, WordPress will store the password set when you installed WordPress.
 
 In the worst case scenario, you may have to use phpMyAdmin or the MySQL command line to [reset a user's password](http://codex.wordpress.org/Resetting_Your_Password).
+
+== Changelog ==
+
+= 2.4 =
+* Add support for WordPress MU (Elliot Kendall)
+* Allow for mixed HTTP and built-in authentication by falling back to wp-login.php (Elliot Kendall)
